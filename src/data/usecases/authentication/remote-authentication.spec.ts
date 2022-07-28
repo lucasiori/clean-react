65b25c99b41b73a241257aa05080987a35809dcb
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-07-28 20:40:02 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,17 +2,19 @@ import { faker } from '@faker-js/faker'
 import { mockAuthentication } from '@/domain/test/mock-authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { AccountModel } from '@/domain/models/account-model'
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
+import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { RemoteAuthentication } from './remote-authentication'
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 }
 
 const makeSut = (url: string = 'any_url'): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
   return {
