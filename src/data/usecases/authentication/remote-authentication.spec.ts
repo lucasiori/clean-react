281e805b3dd86c08090a1fd39ce7196b0649f7f5
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-07-28 20:51:41 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,10 @@
 import { faker } from '@faker-js/faker'
-import { mockAuthentication, mockAccountModel } from '@/domain/test/mock-account'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { mockAuthentication, mockAccountModel } from '@/domain/test'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { HttpStatusCode } from '@/data/protocols/http'
 import { AccountModel } from '@/domain/models/account-model'
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { HttpPostClientSpy } from '@/data/test'
+import { AuthenticationParams } from '@/domain/usecases'
 import { RemoteAuthentication } from './remote-authentication'
 
 type SutTypes = {
