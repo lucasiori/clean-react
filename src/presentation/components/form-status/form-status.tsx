611cf4bfd8c204c5bb902c8d0db4d0a5e445410c
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-07-30 14:34:14 -0300

    refactor: move form status to a separate file

---
+++ src/presentation/components/form-status/form-status.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Spinner from '@/presentation/components/spinner/spinner'
+import Styles from 'login-header-styles.scss'
+
+const FormStatus: React.FC = () => {
+  return (
+    <div className={Styles.errorWrap}>
+      <Spinner className={Styles.spinner} />
+      <span className={Styles.error}>Error</span>
+    </div>
+  )
+}
+
+export default FormStatus
