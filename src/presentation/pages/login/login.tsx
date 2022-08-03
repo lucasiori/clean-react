9afe2c9dcc97b34c5db115f5bbc923f02da4e9ab
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-08-03 20:19:47 -0300

    feat: prevent Login to call submit more than once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -25,6 +25,8 @@ const Login: React.FC<Props> = ({ validation, authentication }) => {
   ): Promise<void> => {
     event.preventDefault()
 
+    if (state.isLoading) return
+
     setState({ ...state, isLoading: true })
     await authentication.auth({ email: state.email, password: state.password })
   }
