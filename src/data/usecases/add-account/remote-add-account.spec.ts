e8deddd36dd742dc7562dd412a278b832aeb8d2a
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-08-27 17:00:20 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -62,4 +62,15 @@ describe('RemoteAddAccount', () => {
 
     await expect(promise).rejects.toThrow(new EmailInUseError())
   })
+
+  test('should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+
+    const promise = sut.add(mockAddAccountParams())
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
