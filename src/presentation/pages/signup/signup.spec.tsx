d3cc45c20f7d59b0deaf508d3cfd67e5832a0ae1
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-08-29 14:17:05 -0300

    test: ensure SignUp show password valid status if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -89,4 +89,12 @@ describe('SignUp Component', () => {
 
     Helper.testStatusForField(sut, 'email')
   })
+
+  test('should show valid password state if Validation succeeds', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut, 'password')
+
+    Helper.testStatusForField(sut, 'password')
+  })
 })
