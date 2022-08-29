492b52f042db14edcdde5bb8d7d13e828d49b8e0
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-08-29 14:13:56 -0300

    feat: ensure SignUp change passwordConfirmation status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -14,11 +14,12 @@ const SignUp: React.FC<Props> = ({ validation }) => {
     name: '',
     email: '',
     password: '',
+    passwordConfirmation: '',
     mainError: '',
     nameError: '',
     emailError: '',
     passwordError: '',
-    passwordConfirmationError: 'Campo obrigatório'
+    passwordConfirmationError: ''
   })
 
   useEffect(() => {
@@ -26,9 +27,13 @@ const SignUp: React.FC<Props> = ({ validation }) => {
       ...state,
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
-      passwordError: validation.validate('password', state.password)
+      passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate(
+        'passwordConfirmation',
+        state.passwordConfirmation
+      )
     })
-  }, [state.name, state.email, state.password])
+  }, [state.name, state.email, state.password, state.passwordConfirmation])
 
   return (
     <div className={Styles.signup}>
