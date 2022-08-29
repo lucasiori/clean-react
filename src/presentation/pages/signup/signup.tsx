343c780014d06b70b9b302ae35544fdb5ce62eb9
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-08-29 14:44:58 -0300

    feat: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -29,7 +29,10 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }) => {
   ): Promise<void> => {
     event.preventDefault()
 
+    if (state.isLoading) return
+
     setState({ ...state, isLoading: true })
+
     await addAccount.add({
       name: state.name,
       email: state.email,
