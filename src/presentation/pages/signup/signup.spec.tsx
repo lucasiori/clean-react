08dbbdd57860b5397e4bb836a3697187a0df6f57
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-08-29 14:12:04 -0300

    test: ensure SignUp change password status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -32,7 +32,7 @@ describe('SignUp Component', () => {
 
     Helper.testStatusForField(sut, 'name', validationError)
     Helper.testStatusForField(sut, 'email', validationError)
-    Helper.testStatusForField(sut, 'password', 'Campo obrigatório')
+    Helper.testStatusForField(sut, 'password', validationError)
     Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
     Helper.testChildCount(sut, 'error-wrap', 0)
     Helper.testButtonIsDisabled(sut, 'submit', true)
@@ -54,5 +54,14 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'email')
 
     Helper.testStatusForField(sut, 'email', validationError)
+  })
+
+  test('should show password error if Validation fails', () => {
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
+
+    Helper.populateField(sut, 'password')
+
+    Helper.testStatusForField(sut, 'password', validationError)
   })
 })
