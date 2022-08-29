4ce92b3f91081451c1e228c8a044f3a3511a86b8
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-08-29 14:16:45 -0300

    test: ensure SignUp show email valid status if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -81,4 +81,12 @@ describe('SignUp Component', () => {
 
     Helper.testStatusForField(sut, 'name')
   })
+
+  test('should show valid email state if Validation succeeds', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut, 'email')
+
+    Helper.testStatusForField(sut, 'email')
+  })
 })
