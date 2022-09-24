4d0caa73c6bb4bbbdf6033cdfcaf4be8dbd4e989
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-09-24 14:54:19 -0300

    test: ensure AxiosHttpClient returns correct value on success

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -57,5 +57,17 @@ describe('AxiosHttpClient', () => {
 
       expect(mockedAxios.get).toHaveBeenCalledWith(request.url)
     })
+
+    test('should return correct response on axios.get', async () => {
+      const { sut, mockedAxios } = makeSut()
+
+      const httpResponse = await sut.get(mockGetRequest())
+      const axiosResponse = await mockedAxios.get.mock.results[0].value
+
+      expect(httpResponse).toEqual({
+        statusCode: axiosResponse.status,
+        body: axiosResponse.data
+      })
+    })
   })
 })
