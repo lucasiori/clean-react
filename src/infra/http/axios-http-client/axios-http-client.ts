e53cb52df11a05f211007059f342f81dc05c5bd6
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-09-24 14:47:05 -0300

    feat: ensure AxiosHttpClient calls axios.get with correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,5 +1,10 @@
 import axios, { AxiosResponse } from 'axios'
-import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
+import {
+  HttpGetParams,
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse
+} from '@/data/protocols/http'
 
 export class AxiosHttpClient implements HttpPostClient {
   async post (params: HttpPostParams): Promise<HttpResponse> {
@@ -15,5 +20,9 @@ export class AxiosHttpClient implements HttpPostClient {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
     }
+  }
+
+  async get (params: HttpGetParams): Promise<void> {
+    await axios.get(params.url)
   }
 }
