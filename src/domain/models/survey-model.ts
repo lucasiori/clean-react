ae8b831ff4af56f2bd67eca6e5b6acfc7ed3cb44
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-09-24 14:25:35 -0300

    feat: ensure RemoteLoadSurveyList returns a list on 200

--- src/domain/models/survey-model.ts
+++ src/domain/models/survey-model.ts
@@ -1,10 +1,12 @@
+type SurveyAnswerModel = {
+  image?: string
+  answer: string
+}
+
 export type SurveyModel = {
   id: string
   question: string
-  answers: [{
-    image?: string
-    answer: string
-  }]
-  date: string
+  answers: SurveyAnswerModel[]
+  date: Date
   didAnswer: boolean
 }
