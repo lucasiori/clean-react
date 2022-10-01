400ddb2fdef6e81e635bccf11a22612cd66f7811
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-10-01 15:17:17 -0300

    refactor: make SetStorage sync function

--- src/data/usecases/update-current-account/local-update-current-account.spec.ts
+++ src/data/usecases/update-current-account/local-update-current-account.spec.ts
@@ -28,7 +28,9 @@ describe('LocalUpdateCurrentAccount', () => {
 
   test('should throw if SetStorage throws', async () => {
     const { sut, setStorageMock } = makeSut()
-    jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
+    jest.spyOn(setStorageMock, 'set').mockImplementationOnce(() => {
+      throw new Error()
+    })
 
     const promise = sut.save(mockAccountModel())
 
