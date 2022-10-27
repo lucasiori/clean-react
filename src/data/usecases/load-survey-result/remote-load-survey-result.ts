8ea889aded20582b857a6546044ea00b83c95bdc
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-10-27 17:05:24 -0300

    feat: ensure RemoteLoadSurveyResult throws UnexpectedError on 404

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,4 +1,4 @@
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
 
 export class RemoteLoadSurveyResult {
@@ -12,7 +12,8 @@ export class RemoteLoadSurveyResult {
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: break
-      default: throw new AccessDeniedError()
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
+      default: throw new UnexpectedError()
     }
   }
 }
