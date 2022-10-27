79b29466f2fb22287374f2b43704d2cd9de0e8d9
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-10-27 16:55:49 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -2,11 +2,23 @@ import { faker } from '@faker-js/faker'
 import { HttpGetClientSpy } from '@/data/test'
 import { RemoteLoadSurveyResult } from './remote-load-survey-result'
 
+type SutType = {
+  sut: RemoteLoadSurveyResult
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutType => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+
+  return { sut, httpGetClientSpy }
+}
+
 describe('RemoteLoadSurveyResult', () => {
   test('should call HttpGetClient with correct url', async () => {
     const url = faker.internet.url()
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+    const { sut, httpGetClientSpy } = makeSut(url)
+
     await sut.load()
 
     expect(httpGetClientSpy.url).toBe(url)
