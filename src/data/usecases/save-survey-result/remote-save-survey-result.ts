21f899411770327da49383d4fd1463bf1bb7bb94
Lucas Fernando Iori <lucasferiori@gmail.com>
2022-11-24 16:20:57 -0300

    feat: ensure RemoteSaveSurveyResult calls HttpClient with correct body

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -11,7 +11,8 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
   async save (params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
     await this.httpClientSpy.request({
       url: this.url,
-      method: 'put'
+      method: 'put',
+      body: params
     })
 
     return null
